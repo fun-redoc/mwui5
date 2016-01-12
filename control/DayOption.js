cfb6ee8876f263b38b14ed6fca3ed04290ea1ed5
Roland <fun.redoc@gmail.com>
2016-01-12 21:03:14 +0100

    value check in day options

--- control/DayOption.js
+++ control/DayOption.js
@@ -25,14 +25,31 @@ sap.ui.define(["sap/ui/core/Control"],
           }
         }
       }),
+      _setViewState : function(hoursEnabled, compleDayEnabled) {
+        var hoursInput          = sap.ui.getCore().byId(this.getId()+"-idDeviantHours");
+        //var hoursInputLabel     = sap.ui.getCore().byId(this.getId()+"-idLabelDeviantHours");
+        var completeDayCheckBox = sap.ui.getCore().byId(this.getId()+"-idCompleteDay");
+
+        hoursInput.setEnabled(hoursEnabled);
+        //hoursInputLabel.setEnabled(hoursEnabled);
+        completeDayCheckBox.setEnabled(compleDayEnabled);
+      },
       metadata : {
                     properties : { day     : {type:"string"},
-                                    options : {type:"object", defaultValue:{ available:false, completeDay:false, deviantHours:"0.0" }}
+                                    options : {type:"object", defaultValue:{ available:false, completeDay:false, deviantHours:"" }}
                                   },
                     aggregations: { _contentPane: {typa:"sap.ui.layout.Grid", multiple:false, visibility:"hidden"}},
                                 events      : {change : {parameters: {value : {type : "object"}}}}
                  },
       init     : function() {
+
+                    this._states = {
+                      initial : this._setViewState.bind(this, false, false),
+                      available : this._setViewState.bind(this, true, true),
+                      completeDay : this._setViewState.bind(this, false, true),
+                      hours: this._setViewState.bind(this, true, false)
+                    };
+                    
                     var grid = new sap.ui.layout.Grid({
                                      defaultSpan:"L12 M12 S12",
                                      width:"auto",
@@ -40,35 +57,38 @@ sap.ui.define(["sap/ui/core/Control"],
                                         new sap.m.Label(this.getId()+"-idDay",{
                                           text:this.getDay(),
                                           textAlign:"Right",
-                                          class:"sapUiSmallMarginTop sapUiSmallMarginBottom",
                                           layoutData:[ new sap.ui.layout.GridData({span:"L2 M2 S5"})]
-                                        }),
+                                        }).addStyleClass("sapUiSmallMarginTop").addStyleClass("sapUiSmallMarginBottom"),
                                         new sap.m.CheckBox(this.getId()+"-idAvailable",{
                                           text:"",
                                           selected:this.getOptions().available,
-                                          select:this._onSelect.bind(this),
+                                          select:this._onSelectDayAvailable.bind(this),
                                           layoutData:[ new sap.ui.layout.GridData({span:"L1 M1 S7"})]
                                         }),
                                         new sap.m.Label(this.getId()+"-idLabalDeviantHours",{
                                           text:"{i18n>deviantHours}",
-                                          class:"sapUiSmallMarginTop sapUiSmallMarginBottom",
                                           layoutData:[ new sap.ui.layout.GridData({span:"L2 M2 S5", linebreakS:true, indentS:2})]
-                                        }),
+                                        }).addStyleClass("sapUiSmallMarginTop").addStyleClass("sapUiSmallMarginBottom"),
                                         new sap.m.Input(this.getId()+"-idDeviantHours",{
                                           value:this.getOptions().deviantHours,
+                                          enabled:false,
                                           change:this._onChange.bind(this),
+                                          liveChange:this._onLiveChange.bind(this),
                                           //type:this.typeHours,
                                           layoutData:[ new sap.ui.layout.GridData({span:"L2 M2 S5"})]
                                         }),
                                         new sap.m.CheckBox(this.getId()+"-idCompleteDay", {
                                           text:"{i18n>completeDay}",
+                                          enabled:false,
                                           selected:this.getOptions().completeDay,
-                                          select:this._onSelect.bind(this),
+                                          select:this._onSelectCompleteDay.bind(this),
                                           layoutData:[ new sap.ui.layout.GridData({span:"L3 M3 S10", linebreakS:true, indentS:2})]
                                         })
                                      ]
                     });
                     this.setAggregation("_contentPane", grid);
+                    this._states.initial();
+                    console.log("language", sap.ui.getCore().getConfiguration().getLanguage());
                  },
       getDay:function() { var day = this.getProperty("day"); console.log("in getDay", day); return day; },
       renderer : function(renderManager, control) {
@@ -83,13 +103,57 @@ sap.ui.define(["sap/ui/core/Control"],
                     renderManager.renderControl(control.getAggregation("_contentPane"));
                     renderManager.write("</div>");
                  },
-      _onSelect : function(evt) {
-        console.log("_onSelect", evt.getSource());
+      _onLiveChange : function(evt) {
+        var self = this;
+        console.log("_onLiveChange");
+        F.Maybe(evt)
+          .obind('getSource')
+          .bind(function(source) {
+            F.Maybe(source)
+              .obind('getParameters')
+              .obind('getValue')
+              .bind(function(value) {
+                if(value.length > 0 ) { self._states.hours(); } else { self._states.available(); }
+                var regex  = /^[0-1]?[0-9]?(\.[0-9]?[0-9]?|$)$/;
+                if (!value.match(regex)) {
+                  //throw new sap.ui.model.SimpleTypeValidateException("'" + oValue + "' is not a valid email address");
+                  source.setValueState(sap.ui.core.ValueState.Error);
+                } else {
+                  source.setValueState(sap.ui.core.ValueState.None);
+                }
+              });
+          });
       },
+      _onSelectCompleteDay : function(evt) {
+        var self = this;
+        console.log("_onSelectCompleteDay");
+        F.Maybe(evt)
+          .obind('getSource')
+          .obind('getSelected')
+          .bind(function(isSelected) {
+            console.log(isSelected);
+            if(isSelected) { self._states.completeDay(); } else { self._states.available(); }
+          });
+      },
+    _onSelectDayAvailable : function(evt) {
+      var self = this;
+      console.log("_onSelectDayAvailable");
+      F.Maybe(evt)
+        .obind('getSource')
+        .obind('getSelected')
+        .bind(function(isSelected) {
+          if(isSelected) { self._states.available();} else { self._states.initial(); }
+        });
+    },
+    _onSelect : function(evt) {
+      console.log("_onSelect", evt.getSource());
+    },
       _onChange: function(evt) {
-        evt.getSource().setValueState(sap.ui.core.ValueState.Error);
-        evt.cancelBubble();
-        evt.preventDefault();
+        // TODO check it value is in the given range 
+        // TODO switch to next .25 mintes
+        //evt.getSource().setValueState(sap.ui.core.ValueState.Error);
+        //evt.cancelBubble();
+        //evt.preventDefault();
      }
     });
   }
